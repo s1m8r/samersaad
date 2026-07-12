ef6cf2f0329b354f4d787758f42c3911d1578408
Samer Salim <s1m8rsa@gmail.com>
2026-07-12 17:18:58 +0300

    edit-04

--- src/pages/storePage/deleteStore.tsx
+++ src/pages/storePage/deleteStore.tsx
@@ -1,5 +1,5 @@
 import { useDeleteStore } from "@/API/store";
-import ContainerDel from "@/components/layout/ContainerDel";
+import ContainerDel from "@/components/layout/containerDel";
 import ConfirmDeleteModal from "@/components/layout/delete";
 import { toast } from "sonner";
 
